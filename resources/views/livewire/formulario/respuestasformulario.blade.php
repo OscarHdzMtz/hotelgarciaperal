f181bdc1d4105b62f77e1f33ef3651a0dc4ca93a
Oscar Hernandez Martinez <oskr9922@gmail.com>
2023-06-16 15:48:01 -0600

    pequeños cambios

--- resources/views/livewire/formulario/respuestasformulario.blade.php
+++ resources/views/livewire/formulario/respuestasformulario.blade.php
@@ -44,7 +44,7 @@
                                 </svg>
                             </div>
                         </label>
-                        <label class="mt-px font-light text-gray-700 cursor-pointer select-none text-blue-gray-700 " for="html">
+                        <label class="mt-px font-light text-gray-700 cursor-pointer select-none" for="html">
                             <small>{{ $valorcomponenteArray[$radio] }}</small>
                         </label>
                     </div>
@@ -79,14 +79,16 @@
         @elseif($preguntas->tipodecomponente === 'select')
             <div class="relative h-10 w-72 min-w-[200px]">
                 <select
-                    class="block w-full px-4 py-3 text-sm border-green-500 rounded-md pr-9 focus:border-green-600 focus:ring-green-500 "
-                    required>
+                    class="block w-full px-4 py-3 text-sm border-green-500 rounded-md pr-9 focus:border-green-600 focus:ring-green-600" wire:model='valoresRespuesta'>
                     <option value="">Selccione una opcion</option>
                     @for ($select = 0; $select < $preguntas->numerodecomponente; $select++)
-                        <option value="{{ $valorcomponenteArray[$select] }}">{{ $valorcomponenteArray[$select] }}
-                        </option>
+                        <option value="{{ $valorcomponenteArray[$select] }}">{{ $valorcomponenteArray[$select] }}</option>
                     @endfor
                 </select>
+                {{-- <label
+                    class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                    Select a City
+                </label> --}}
             </div>
         @endif
     </div>
