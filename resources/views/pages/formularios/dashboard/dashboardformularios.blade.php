fda455577f7b3ab976e69de768f2c4dcf9c8cff2
oskrHM <oskr9922@gmail.com>
2024-11-04 18:07:10 -0600

    cambios temporales 4

--- resources/views/pages/formularios/dashboard/dashboardformularios.blade.php
+++ resources/views/pages/formularios/dashboard/dashboardformularios.blade.php
@@ -8,13 +8,13 @@
         <div class="mb-8 sm:flex sm:justify-between sm:items-center">
 
             <!-- Left: Avatars -->
-            <x-dashboard.dashboard-avatars />
+            {{-- <x-dashboard.dashboard-avatars /> --}}
 
             <!-- Right: Actions -->
             <div class="grid justify-start grid-flow-col gap-2 sm:auto-cols-max sm:justify-end">
 
                 <!-- Filter button -->
-                <x-dropdown-filter align="right" />
+                {{-- <x-dropdown-filter align="right" /> --}}
 
                 <!-- Datepicker built with flatpickr -->
                 <x-datepicker />
@@ -24,7 +24,7 @@
                     <svg class="w-4 h-4 opacity-50 fill-current shrink-0" viewBox="0 0 16 16">
                         <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
                     </svg>
-                    <span class="hidden ml-2 xs:block">Add View</span>
+                    <span class="hidden ml-2 xs:block">Agragar modulo</span>
                 </button>
                 
             </div>
